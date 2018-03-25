16b5f36a50d2c65335b14b5f9087060ebc8e0611
Guilhem Saurel <guilhem.saurel@gmail.com>
2018-03-25 01:44:24 +0100

    graph ok

---
+++ djenealog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'djenealog'
+urlpatterns = [
+    path('', views.gv),
+]
--- djenealog/views.py
+++ djenealog/views.py
@@ -1,3 +1,13 @@
+from datetime import date
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Individu, Couple
+
+
+def gv(request):
+    return render(request, 'djenealog/graph.gv', {
+        'years': range(Individu.objects.exclude(naissance_y__lt=1800).order_by('naissance_y').first().naissance_y,
+                       date.today().year + 1),
+        'individus': Individu.objects.all(),
+        'couples': Couple.objects.all(),
+    })

277d15be159d6a63497e9d0d8d1ca880472f7184
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-12-24 16:53:05 +0100

    ndh now requires i18n

--- testproject/urls.py
+++ testproject/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path("select2/", include("django_select2.urls")),
+    path("i18n/", include("django.conf.urls.i18n")),
     path("", include("djenealog.urls")),
 ]
